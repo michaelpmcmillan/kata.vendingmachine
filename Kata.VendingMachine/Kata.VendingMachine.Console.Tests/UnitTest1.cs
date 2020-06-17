7e50e03fa8accac6037cad846cb493e520f6b0f1
Mike <mike.mcmillan@onthebeach.co.uk>
2020-06-17 10:50:33 +0100

    Add kata skeleton

---
+++ Kata.VendingMachine/Kata.VendingMachine.Console.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Kata.VendingMachine.Console.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Given_When_Then()
+        {
+
+        }
+    }
+}
---
+++ Kata.VendingMachine/Kata.VendingMachine.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Kata.VendingMachine.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
